c5781b463568a089ce12df9b5b316ff8fde5a9de
nitisyad@in.ibm.com <nitisyad@in.ibm.com>
2017-08-22 20:27:44 +0530

    new prompt file added

--- inputExcercise.py
+++ inputExcercise.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 age = input("How old are you?")
 height=int(input("What is your height in cm:"))
+#agrv means agrument vector, from sys import import argv feature
+from sys import argv
+
+#these following line help to get the command line argument
+script,first,second,third =argv
+
+print("The script is called:",script)
+print("Your first cmd arg variable is:",first)
+print("Your second cmd arg variable is:",second)
+print("Your third cmd arg variable is:",third)
---
+++ prompt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#How to prompt people for infornmation from command line or regular input style
+from sys import argv
+
+script,user_name =argv
+prompt = '>'
+
+print(f"hi {user_name},I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
