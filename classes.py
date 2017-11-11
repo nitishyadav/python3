e04cc1565eebb4230456ded030c39f8932fd06e1
nitisyad@in.ibm.com <nitisyad@in.ibm.com>
2017-11-11 13:54:17 +0530

    example of class

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Employee:
+    name ="Nitish"
+    designation = "Cloud Automation Engineer"
+    workedThisWeek = 45
+    def hasAchievedTarget(self):
+        if self.workedThisWeek >=40:
+            print("Target achieved")
+        else:
+            print("target missed")
+        
+employee1 = Employee()            
+employee1.hasAchievedTarget()
+print(employee1.name)
