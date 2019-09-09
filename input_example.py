dc606cf50ee5abd10c0b8c67c67312a431eba82a
Yadav <nitishyadav.2392@gmail.com>
2019-09-09 11:02:40 +0530

    removed unwanted files

---
+++ input_example.py
@@ -0,0 +1,16 @@
+#The input() function returns a string value. If you want a number value, simply write the 'int' keyword before input()
+
+str1 = input("Enter a String: ")
+print("Entered string is: ", str1)
+print()
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = a + b
+print("Value of c is: ", c)
+print()
+
+num1 = float(input("Enter num 1: "))
+num2 = float(input("Enter num 2: "))
+num3 = num1/num2
+print("value of num3 is: ", num3)
